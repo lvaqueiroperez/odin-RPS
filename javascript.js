6a31fd138b069d296c5aac5bbb3859ad6d3ad683
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-03-03 11:30:12 +0100

    Create getComputerChoice function Function that allows to get Rock, Paper or Scissors from the computer randomly.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,24 @@
 console.log("hello world!");
+
+function getComputerChoice() {
+    const rpsNumber = Math.round(Math.random() * 2) + 1;
+    let rps = "";
+
+    switch (rpsNumber) {
+        case 1:
+            rps = "Rock"
+            break;
+        case 2:
+            rps = "Paper"
+            break;
+        case 3:
+            rps = "Scisors"
+            break;
+        default:
+            rps = "An error has occurred."
+
+    }
+
+    return rps;
+
+}
