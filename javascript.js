0465c84e0bccb8a6928754c8011b2beea91a627c
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-03-13 18:43:09 +0100

    Move playRound function outside partent function

--- javascript.js
+++ javascript.js
@@ -1,13 +1,17 @@
 console.log(`Enter "playGame()" to start!`);
 
+let computerScore = 0;
+let userScore = 0;
+
 const btnDiv = document.querySelector("#btnContainer");
 
 btnDiv.addEventListener("click", (e) => {
     const btnId = e.target.id;
     console.log(btnId);
 
-    if(btnId === "rock" || btnId === "paper" || btnId === "scissors") {
-        playRound(getComputerChoice, btnId);
+    if (btnId === "rock" || btnId === "paper" || btnId === "scissors") {
+        // quiero el resultado/return de la función, por eso pongo los (), no es una callback !!!
+        playRound(getComputerChoice(), btnId);
     } else {
         console.log("No button was clicked.");
     }
@@ -38,6 +42,7 @@ function getComputerChoice() {
 
 }
 
+/*
 function getHumanChoice() {
     let humanChoice = prompt("Write and submit:\nRock, Paper, Scissors");
 
@@ -60,68 +65,65 @@ function getHumanChoice() {
 
 
 }
+*/
+
+function playRound(computerSelection, humanSelection) {
+
+    //console.log("HUMAN: " + humanSelection);
+    //console.log("COMPUTER: " + computerSelection);
+    console.log("**************************************************");
+    if (!humanSelection) {
+        console.log("An invalid value has been entered, you lose!");
+        computerScore++;
+    } else if (computerSelection === humanSelection) {
+        console.log("Computer: " + computerSelection);
+        console.log("You: " + humanSelection);
+        console.log("It's a tie!");
+    } else if (computerSelection === "scissors" && humanSelection === "paper") {
+        console.log("Computer: " + computerSelection);
+        console.log("You: " + humanSelection);
+        console.log("You lose!");
+        computerScore++;
+    }
+    else if (computerSelection === "scissors" && humanSelection === "rock") {
+        console.log("Computer: " + computerSelection);
+        console.log("You: " + humanSelection);
+        console.log("You win!");
+        userScore++;
+    }
+    else if (computerSelection === "paper" && humanSelection === "rock") {
+        console.log("Computer: " + computerSelection);
+        console.log("You: " + humanSelection);
+        console.log("You lose!");
+        computerScore++;
+    }
+    else if (computerSelection === "paper" && humanSelection === "scissors") {
+        console.log("Computer: " + computerSelection);
+        console.log("You: " + humanSelection);
+        console.log("You win!");
+        userScore++;
+    }
+    else if (computerSelection === "rock" && humanSelection === "paper") {
+        console.log("Computer: " + computerSelection);
+        console.log("You: " + humanSelection);
+        console.log("You win!");
+        userScore++;
+    }
+    else if (computerSelection === "rock" && humanSelection === "scissors") {
+        console.log("Computer: " + computerSelection);
+        console.log("You: " + humanSelection);
+        console.log("You lose!");
+        computerScore++;
+    }
+
+    console.log("Scores:\nUser: " + userScore + "\nComputer: " + computerScore);
+
+}
 
 // pendiente: arreglar comportamiento cuando el user escribe algo mal, repetir esa llamada a la función hasta que lo que introduzca el user esté bien
 // también podría dejarlo así, pero hacer otra alternativa sirve para practicar
 // Además, al final de este proyecto se podría refactorizar para que quede bonito. Recuerda, una función por cada acción.
 function playGame() {
-
-    let computerScore = 0;
-    let userScore = 0;
-
-    // no me gusta que esto esté aquí declarado, pero lo ponía en los apuntes
-    function playRound(computerSelection, humanSelection) {
-
-        //console.log("HUMAN: " + humanSelection);
-        //console.log("COMPUTER: " + computerSelection);
-
-        if (humanSelection === "Invalid value") {
-            console.log("An invalid value has been entered, you lose!");
-            computerScore++;
-        } else if (computerSelection === humanSelection) {
-            console.log("Computer: " + computerSelection);
-            console.log("You: " + humanSelection);
-            console.log("It's a tie!");
-        } else if (computerSelection === "scissors" && humanSelection === "paper") {
-            console.log("Computer: " + computerSelection);
-            console.log("You: " + humanSelection);
-            console.log("You lose!");
-            computerScore++;
-        }
-        else if (computerSelection === "scissors" && humanSelection === "rock") {
-            console.log("Computer: " + computerSelection);
-            console.log("You: " + humanSelection);
-            console.log("You win!");
-            userScore++;
-        }
-        else if (computerSelection === "paper" && humanSelection === "rock") {
-            console.log("Computer: " + computerSelection);
-            console.log("You: " + humanSelection);
-            console.log("You lose!");
-            computerScore++;
-        }
-        else if (computerSelection === "paper" && humanSelection === "scissors") {
-            console.log("Computer: " + computerSelection);
-            console.log("You: " + humanSelection);
-            console.log("You win!");
-            userScore++;
-        }
-        else if (computerSelection === "rock" && humanSelection === "paper") {
-            console.log("Computer: " + computerSelection);
-            console.log("You: " + humanSelection);
-            console.log("You win!");
-            userScore++;
-        }
-        else if (computerSelection === "rock" && humanSelection === "scissors") {
-            console.log("Computer: " + computerSelection);
-            console.log("You: " + humanSelection);
-            console.log("You lose!");
-            computerScore++;
-        }
-
-        console.log("Scores:\nUser: " + userScore + "\nComputer: " + computerScore);
-
-    }
 
     let winner = "";
 
